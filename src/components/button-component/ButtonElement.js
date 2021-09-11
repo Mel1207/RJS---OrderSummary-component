3abe25a0aa8d19801be4053f64c174b7e68183ae
Mel <meljohnpualon@gmail.com>
2021-09-11 20:56:24 +0800

    added button components

---
+++ src/components/button-component/ButtonElement.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const BtnPrimary = styled.button`
+    color: #fff;
+    background: green;
+    outline: none;
+    border: none;
+    padding: 10px;
+`
+
+const BtnSecondary = styled.button`
+    color: #fff;
+    background: blueviolet;
+    outline: none;
+    border: none;
+    padding: 10px;
+`
+
+export {BtnPrimary, BtnSecondary}
--- src/components/card-component/index.js
+++ src/components/card-component/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { CardStyled, CardImg, CardH1, CardText, CardBody } from './CardElement'
 import CardAvatar from './../../images/illustration-hero.svg'
+import { BtnPrimary, BtnSecondary } from '../button-component/ButtonElement'
+
 
 
 const Card = () => {
@@ -11,6 +13,8 @@ const Card = () => {
                 <CardBody> 
                     <CardH1 >Order Summary</CardH1>
                     <CardText>You can now listen to millions of songs, <br /> audiobooks, and podcast on any device <br /> anywhere you like!</CardText>
+                    <BtnSecondary>This is Secondary</BtnSecondary>
+                    <BtnPrimary>This is primary</BtnPrimary>
                 </CardBody>
             </CardStyled>
             
