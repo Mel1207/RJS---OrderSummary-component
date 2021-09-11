a7f99d5b8b48bb1387b93a57ddfe5e872014d191
Mel <meljohnpualon@gmail.com>
2021-09-12 02:34:12 +0800

    fix item detail content ui

--- src/components/music-plan-component/index.js
+++ src/components/music-plan-component/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import { MusicItem } from './music-plan-elements'
+import { MusicItem, MusicIcon, MusicPlanName, MusicPrice, MusicPlanDetails } from './music-plan-elements'
 import MscIcon from './../../images/icon-music.svg'
 
 const MusicPlanItem = () => {
     return (
         <MusicItem>
-            <img src={MscIcon} />
-            <MusicDetails>
-                <MusicPlanName>Annual Plan</MusicPlanName>
-                <MusicPrice>$59.99/year</MusicPrice>
-            </MusicDetails>
+            <MusicPlanDetails>
+                <MusicIcon src={MscIcon} />
+                <div>
+                    <MusicPlanName>Annual Plan</MusicPlanName>
+                    <MusicPrice>$59.99/year</MusicPrice>
+                </div>
+            </MusicPlanDetails>
             <a href="#!">Change</a>
         </MusicItem>
     )
--- src/components/music-plan-component/music-plan-elements.js
+++ src/components/music-plan-component/music-plan-elements.js
@@ -3,7 +3,31 @@ import styled from "styled-components";
 const MusicItem = styled.div`
     border-radius: 8px;
     background: #F5F7FF;
-    padding: 10px;
+    padding: 13px 18px;
     margin: 15px 0 20px 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 `
-export {MusicItem}
+
+const MusicPlanDetails = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const MusicIcon = styled.img`
+    height: 40px;
+    width: 40px;
+    margin-right: 12px;
+`
+
+const MusicPlanName = styled.p`
+    color: #1F2F56;
+    font-weight: 900;
+`
+
+const MusicPrice = styled.p`
+    color: #7280A7;
+`
+
+export {MusicItem, MusicPlanName, MusicPrice, MusicPlanDetails, MusicIcon}
